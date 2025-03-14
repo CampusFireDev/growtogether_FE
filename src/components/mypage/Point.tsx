7fa36fb15110f3768d506a98612965a8f4258f7c
shinwonkim <shinwonkkim@gmail.com>
2025-03-14 10:33:39 +0900

    feat: 마이페이지 및 포인트 페이지 레이아웃

---
+++ src/components/mypage/Point.tsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import FormButton from "../form/FormButton";
+
+const Point = () : JSX.Element =>{
+    const transactions = [
+        {date: "2025-02-01", type: "적립", amount: 1000},
+        {date: "2025-02-02", type: "사용", amount: -1000},
+        {date: "2025-02-03", type: "충전", amount: 1000},
+        {date: "2025-02-04", type: "적립", amount: 1000},
+        {date: "2025-02-05", type: "적립", amount: 1000},
+        {date: "2025-02-05", type: "적립", amount: 1000},
+    ]
+    const getAmount = (amount: number) =>amount > 0 ? "text-myBlue" : "text-[#FF0000]";
+
+
+    return (
+        <>
+            <div className="w-full flex justify-around items-center gap-60 h-30 p-5 rounded-[20px] shadow-[0px_10px_20px_rgba(0,0,0,0.25)] bg-white my-10 overflow-hidden">
+                <div className="flex gap-3 items-center nexon-bold">
+                    <div className="w-8 h-8 mx-3 flex justify-center items-center text-white rounded-full bg-black ">P</div>
+                    <p className="nexon-bold">현재 사용가능한 포인트:</p>
+                    <div className="flex items-center justify-end gap-1">
+                        <p className="text-myGreen text-[28px]">82</p>
+                        <p>P</p>
+                    </div>
+                </div>
+                <div>
+                    <Link to="/mypage/point/charge">
+                        <FormButton type="button" className="bg-myGreen px-10 rounded-full nexon-medium">포인트 충전</FormButton>
+                    </Link>
+                </div>
+            </div>
+            <div className="my-20 ">
+                <p className="nexon-bold mb-2">포인트 내역</p>
+                <div className="border-y-2 border-black9 nexon-medium">
+                    {/* 테이블 헤더 */}
+                    <div className="grid grid-cols-3 items-center h-[50px] border-b-1 border-gray9 bg-white5 text-black4 text-center">
+                        <p>발생 일자</p> <p>거래 구분</p><p>금액</p>
+                    </div>
+                   
+                    {transactions.map((item, index) => (
+                        <div key={index} className="grid grid-cols-3 items-center border-b border-gray5 h-[60px] text-center text-[14px] text-black4">
+                            <p>{item.date}</p>
+                            <p>{item.type}</p>
+                            <p className={`${getAmount(item.amount)}`}>{item.amount > 0 ? `+${item.amount}` : item.amount}</p>
+                        </div>
+                    ))} 
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default Point;
---
+++ src/components/mypage/PointCharge.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { FaRegCircleCheck } from "react-icons/fa6";
+import FormButton from "../form/FormButton";
+import KakaoPayButton from "./KakaoPayButton";
+const PointCharge = (): JSX.Element => {
+    const [selectedPoint, setSelectedPoint] = useState<number | null>(null);
+    const [customPoint, setCustomPoint] = useState<number | null>(null);
+    const [payment, setPayment] = useState<number>(0);
+    const pointList = [
+        { amount: 0 },
+        { amount: 1000 },
+        { amount: 2000 },
+        { amount: 3000 },
+        { amount: 4000 },
+        { amount: 5000 },
+    ];
+    
+    const handlePointClick = (amount: number) => {
+        setSelectedPoint(amount);
+        if (amount === 0) setCustomPoint(0); // 직접 입력 선택 시 초기화
+        setPayment(amount);
+    };
+
+    const handleCustomPointChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value.replace(/\D/g, "");
+        const numberValue = value ? parseInt(value, 10) : null;
+        setCustomPoint(numberValue);
+        setSelectedPoint(0); // 직접 입력한 경우, 선택된 포인트를 0으로 설정
+        setPayment(numberValue || 0);
+    };
+
+    return (
+        <div className="text-center w-full max-w-[800px] mx-auto">
+            <h2 className="text-[20px] my-5 nexon-bold">포인트 충전하기</h2>
+            <div className="border-2 border-gray5 rounded-[15px] text-[14px] nexon-medium">
+                {pointList.map((item) => (
+                    <div key={item.amount}
+                        className={`grid grid-cols-2 items-center py-5 px-10 border-b last:border-b-0 border-gra text-black4 nexon-medium
+                            ${selectedPoint === item.amount ? "bg-white5" : ""}`
+                        }
+                        onClick={() => handlePointClick(item.amount)}
+                    >
+                        <div className="flex gap-3 items-center cursor-pointer">
+                            <FaRegCircleCheck className={selectedPoint === item.amount ? "text-myGreen" : "text-gray-400"} />
+                            {item.amount !== 0 ? (
+                                <p>{item.amount} P</p>
+                            ) : (
+                                <input type="number" placeholder="직접 입력하기" className="w-[120px] border-none focus:outline-none px-2 rounded" 
+                                    value={customPoint ?? ""} onChange={handleCustomPointChange} onClick={(e) => e.stopPropagation()}
+                                />
+                            )}
+                        </div>
+                        {item.amount !== 0 && (
+                            <p className="text-right">결제 금액 {item.amount.toLocaleString()} 원</p>
+                        )}
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <p className="nexon-bold my-7 text-right mr-10 text-black4">
+                    충전 후 포인트: <span className="text-myGreen">{selectedPoint || customPoint || 0} P</span>
+                </p>
+            </div>
+
+            {/* 카카오페이 결제 버튼 */}
+            <div className="flex justify-center items-center my-5">
+                {/* <FormButton type="submit" className="!bg-[#FEE500] flex justify-center items-center !w-[300px]">
+                    <img src="/images/kakao_logo.png" className="w-10" alt="카카오페이 로고" />
+                    <p className="text-black">카카오페이로 결제하기</p>
+                </FormButton> */}
+                <KakaoPayButton amount={payment} />
+            </div>
+        </div>
+    );
+};
+
+export default PointCharge;
