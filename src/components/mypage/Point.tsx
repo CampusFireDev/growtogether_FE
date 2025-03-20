6b99f1557e0c9e82a7df52dba1167f72da31adf8
tiyu25 <tiyuuuu9@email.com>
2025-03-20 11:45:18 +0900

    fix: 충돌 해결

--- src/components/mypage/Point.tsx
+++ src/components/mypage/Point.tsx
@@ -1,57 +1,57 @@
-import { useNavigate } from "react-router-dom";
-import FormButton from "../form/FormButton";
+import ButtonStyle1 from "../common/ui/ButtonStyle1";
+import usePointHistory from "../../hooks/mypage/usePointHistory";
+import useMyPageInfo from "../../hooks/mypage/useMyPageInfo";
 
 const Point = () : JSX.Element =>{
-    const currentPoints = 82; 
-    const navigate = useNavigate();
+    const { points, loading } = usePointHistory();
 
-    const transactions = [
-        {date: "2025-02-01", type: "적립", amount: 1000},
-        {date: "2025-02-02", type: "사용", amount: -1000},
-        {date: "2025-02-03", type: "충전", amount: 1000},
-        {date: "2025-02-04", type: "적립", amount: 1000},
-        {date: "2025-02-05", type: "적립", amount: 1000},
-        {date: "2025-02-05", type: "적립", amount: 1000},
-    ]
-    const getAmount = (amount: number) =>amount > 0 ? "text-myBlue" : "text-[#FF0000]";
-
-    const handleChargeClick = () =>{
-        navigate("/mypage/point/charge", {state: {currentPoints}});
-    }
+    // 회원 정보 가져오기
+    const { info } = useMyPageInfo();
 
     return (
         <>
-            <div className="w-full flex justify-around items-center gap-60 h-30 p-5 rounded-[20px] shadow-[0px_10px_20px_rgba(0,0,0,0.25)] bg-white my-10 overflow-hidden">
-                <div className="flex gap-3 items-center nexon-bold">
-                    <div className="w-8 h-8 mx-3 flex justify-center items-center text-white rounded-full bg-black ">P</div>
-                    <p className="nexon-bold">현재 사용가능한 포인트:</p>
-                    <div className="flex items-center justify-end gap-1">
-                        <p className="text-myGreen text-[28px]">{currentPoints}</p>
-                        <p>P</p>
+            <strong className="block nexon-bold text-xl text-black4 mb-2">포인트</strong>
+            <div className="flex flex-col border border-gray5 bg-white min-h-[523px]">
+                <div className="flex items-center justify-between py-7 px-7 ">
+                    <div>
+                        <em className="block nexon text-black6 mr-2 mb-1">
+                            현재 사용 가능한 포인트
+                        </em>
+                        <strong className="nexon-bold text-3xl text-black4">
+                            <span className="text-myBlue">{info?.points}</span>P
+                        </strong>
                     </div>
+                    <ButtonStyle1
+                        type="button"
+                        label="포인트 충전"
+                        className="bg-myBlue text-white"
+                    />
                 </div>
-                <div>
-                    <button onClick={handleChargeClick}>
-                        <FormButton type="button" className="bg-myGreen px-10 rounded-full nexon-medium">포인트 충전</FormButton>
-                    </button>
-                </div>
-            </div>
-            <div className="my-20 ">
-                <p className="nexon-bold mb-2">포인트 내역</p>
-                <div className="border-y-2 border-black9 nexon-medium">
-                    {/* 테이블 헤더 */}
-                    <div className="grid grid-cols-3 items-center h-[50px] border-b-1 border-gray9 bg-white5 text-black4 text-center">
-                        <p>발생 일자</p> <p>거래 구분</p><p>금액</p>
+                { loading ? (
+                    <p>포인트 내역을 불러오는 중</p>
+                ): points.length === 0 ? (
+                    <div className="flex-grow flex items-center justify-center border-t border-gray5">
+                        <p className="text-sm text-black6 text-center">내역이 없습니다.</p>
                     </div>
-                   
-                    {transactions.map((item, index) => (
-                        <div key={index} className="grid grid-cols-3 items-center border-b border-gray5 h-[60px] text-center text-[14px] text-black4">
-                            <p>{item.date}</p>
-                            <p>{item.type}</p>
-                            <p className={`${getAmount(item.amount)}`}>{item.amount > 0 ? `+${item.amount}` : item.amount}</p>
-                        </div>
-                    ))} 
-                </div>
+                ): (
+                    <div>
+                        {points.map((point, index) => (
+                            <div className="flex items-center justify-between px-5 py-5 border-t border-gray5">
+                                <div>
+                                    <strong className="block text-lg nexon-medium text-black4">
+                                        {
+                                            point.type === "REWARD" ? "적립" :
+                                            point.type === "USE" ? "사용" : "충전"
+                                        }
+                                    </strong>
+                                    <span className="block text-sm text-black9">{ point.date }</span>
+                                </div>
+                                <strong className="block text-lg nexon-bold text-myBlue">{ point.amount }</strong>
+                            </div>
+                        ))}
+                    </div>
+                )}
+                
             </div>
         </>
     )
--- src/layout/MypageLayout.tsx
+++ src/layout/MypageLayout.tsx
@@ -1,14 +1,70 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Nav from "./Nav";
+import { IoIosArrowForward } from "react-icons/io";
+import useMemberId from "../hooks/auth/\buseMemberId";
+import useMyPageInfo from "../hooks/mypage/useMyPageInfo";
 
 const MypageLayout = ():JSX.Element => {
+    // 아이디 가져오기
+    const memberId = useMemberId();
+
+    // 회원 정보 가져오기
+    const { info, loading, error } = useMyPageInfo();
+
     return (
         <div className="flex flex-col min-h-screen">
             <Nav />
             <div id="content-wrapper" className="bg-white9 flex-grow">
                 <div className="w-full max-w-[1200px] mx-auto mt-[69px] py-[60px]">
-                    <Outlet />
+                    <div className="flex gap-10 justify-content text-nowrap">
+                        {/* 왼쪽 메뉴 */}
+                        <div className="flex flex-col border border-gray5 w-[250px] self-start bg-white nexon-medium text-black4">
+                            <div className="flex flex-col pt-[30px] pb-[20px] px-[20px] border-b border-gray5 justify-center items-center">
+                                <div className="block w-[80px] h-[80px] overflow-hidden mb-3">
+                                    <img src={info?.profileImageUrl} alt=""/>
+                                </div>
+                                <p className="nexon-bold">{info?.nickName}</p>
+                                <p className="text-black6 nexon text-sm mt-1">test@gmail.com</p>
+                                <div className="flex justify-between items-center w-full bg-white5 text-[14px] px-4 py-2 rounded-full mt-4">
+                                    <p>
+                                        내 기술스택 
+                                        <span className="nexon">
+                                            {info?.skills?.map((skill) => skill).join(",")}
+                                        </span>
+                                    </p>
+                                    <IoIosArrowForward size={13} className="shrink" />
+                                </div>
+                            </div>
+                            <Link to="/mypage/point">
+                                <div className="flex gap-3 p-5 border-b-1 border-gray5 justify-between items-center">
+                                    <span className="text-[16px] nexon">포인트</span>
+                                    <span className="nexon-bold">{info?.points}P</span>   
+                                </div>
+                            </Link>
+                            <div className="flex flex-col gap-3 p-5 border-b-1 border-gray5">
+                                <div className="flex justify-between items-center">
+                                    <p className="text-[16px] nexon">내 스터디</p>
+                                    <p className="nexon-bold">2</p>   
+                                </div>
+                                <div className="flex justify-between items-center">
+                                    <p className="text-[16px] nexon">좋아요 게시글</p>
+                                    <p className="nexon-bold">256</p>   
+                                </div>
+                            </div>
+                            <Link to="/mypage/personalinfo">
+                                <div className="flex gap-3 p-4 border-b-1 border-gray5 justify-between items-center">
+                                    <span className="text-[16px]">내 정보 관리</span> 
+                                </div>
+                            </Link>
+                            <div className="flex gap-3 p-4 border-gray5 justify-between items-center">
+                                <p className="text-[16px] text-[#F74175]">로그아웃</p> 
+                            </div>
+                        </div>
+                        <div className="flex-1">
+                            <Outlet />
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer />
