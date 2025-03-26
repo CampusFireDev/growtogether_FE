5ef82dc2d40ce0ab8fafa2b6b78c4e25050c8570
tiyu25 <tiyuuuu9@email.com>
2025-03-27 04:39:40 +0900

    design: 노이미지 추가

--- src/components/study/StudyCard.tsx
+++ src/components/study/StudyCard.tsx
@@ -26,8 +26,10 @@ const StudyCard = ({ study, showTechStack = true }: StudyCardProps): JSX.Element
                 {/* showTechStack이 true일 때만 기술 스택 표시*/}
                 {showTechStack && <TechStackList stacks={study.skillNames}/>}
                 <div className="flex justify-between mt-[15px] pt-[15px] border-t border-gray5">
-                    <div className="flex items-center gap-1">
-                        <div className="w-[20px] h-[20px] rounded-full overflow-hidden bg-gray5"></div>
+                    <div className="flex items-center gap-2">
+                        <div className="w-[20px] h-[20px] rounded-full overflow-hidden bg-gray5">
+                            <img src="/images/noImage20.png" />
+                        </div>
                         <p className="text-sm text-black6">{study.author}</p>
                     </div>
                     <ul className="flex text-sm text-black6 gap-3">
