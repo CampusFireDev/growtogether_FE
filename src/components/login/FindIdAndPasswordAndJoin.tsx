a14b5eaa28db51fae7f2da6285707e538a447c3e
tiyu25 <tiyuuuu9@email.com>
2025-03-27 19:13:08 +0900

    fix: 아이디 찾기 주석

--- src/components/login/FindIdAndPasswordAndJoin.tsx
+++ src/components/login/FindIdAndPasswordAndJoin.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const FindIdAndPassword = (): JSX.Element => {
     return (
         <ul className="flex items-center justify-center mt-4">
-            <li><Link to="/findId" className="nexon text-sm">아이디 찾기</Link></li>
-            <li className="text-xs/[11px] ml-2 mr-2">|</li>
+            {/* <li><Link to="/findId" className="nexon text-sm">아이디 찾기</Link></li> */}
+            {/* <li className="text-xs/[11px] ml-2 mr-2">|</li> */}
             <li><Link to="/findpassword" className="nexon text-sm">비밀번호 찾기</Link></li>
             <li className="text-xs/[11px] ml-2 mr-2">|</li>
             <li><Link to="/signup" className="nexon text-sm">회원가입</Link></li>
